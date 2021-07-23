eee7f2eedd8941118d4c3287b73e8cc6c0100326
rylieohashi <xxryliex@gmail.com>
2021-07-23 09:34:46 -0400

    first commit

---
+++ src/components/app.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div className='home'>
+        <h1>S. E. Shires</h1>
+        <h2></h2>
+      </div>
+    );
+  }
+}
